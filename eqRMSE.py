b52ff0ab21abac83b02bc38a9af120ae25db5a64
Federico Moretta <federicomoretta97@gmail.com>
2022-05-08 12:59:52 +0200

    linear regression added in solver package

--- eqRMSE.py
+++ eqRMSE.py
@@ -11,11 +11,13 @@ def RMSE(Data: List, Model: List) -> float:
     return RMSE
 
 def Methane_RMSE(k6, alpha, D, X2, qM):
-    # Equation for methane flow
-    # qM = k6 * alpha * D * X2
-    #  y = m * x + b
-    #  y = qM | x = X2 | m = k6 * alpha * D
-    # k6 = m / alpha * D
+    """
+    Equation for methane flow
+    qM = k6 * alpha * D * X2
+    y = m * x + b
+    y = qM | x = X2 | m = k6 * alpha * D
+    k6 = m / alpha * D
+    """
     
     y  = np.empty(len(X2))
     m  = np.empty(len(X2))
@@ -30,11 +32,13 @@ def Methane_RMSE(k6, alpha, D, X2, qM):
     
     return RMSE(y, mx)
 
-def Carbon_Dioxide_RMSE(kLa, C, pH, kH, Pc, Kb, qC):
-    # Equation for carbon dioxide flow
-    # qC = kLa * (C / (1 + 10**(pH - pKb)) - kH * Pc)
-    # y = m * x + b
-    # y = qC | x = C / (1 + 10**(pH - pKb)) - kH * Pc | m = kLa
+def Carbon_Dioxide_RMSE(kLa, C, pH, KH, PC, Kb, qC):
+    """ 
+    Equation for carbon dioxide flow
+    qC = kLa * (C / (1 + 10**(pH - pKb)) - kH * Pc)
+    y = m * x + b
+    y = qC | x = C / (1 + 10**(pH - pKb)) - kH * Pc | m = kLa
+    """
     
     y  = np.empty(len(C))
     m  = np.empty(len(C))
@@ -46,16 +50,43 @@ def Carbon_Dioxide_RMSE(kLa, C, pH, kH, Pc, Kb, qC):
     for i in range(0, len(C)):
         y[i]  = qC[i]
         m[i]  = kLa
-        x[i]  = C[i] * (1 / (1 + 10**(pH[i] - pKb))) - kH * Pc[i]
+        x[i]  = C[i] * (1 / (1 + 10**(pH[i] - pKb))) - KH * PC[i]
         mx[i] = m[i] * x[i]
     
     return RMSE(y, mx)
 
+def Carbon_Dioxide_LIN(C, pH, KH, PC, Kb, qC):
+    """ 
+    Equation for carbon dioxide flow
+    qC = kLa * (C / (1 + 10**(pH - pKb)) - kH * Pc)
+    y = m * x + b
+    y = qC | x = C / (1 + 10**(pH - pKb)) - kH * Pc | m = kLa
+    """
+    pKb = -np.log10(Kb)
+    Y = qC
+    
+    if type(pH) is not list:
+        exp =  float(pH) - float(pKb)
+        X = C*1/(1+10**(pH-pKb)) - KH*PC
+    
+    else:
+        exp = np.empty(shape=(len(pH),))
+        X = np.empty(shape=(len(pH),))
+        for i in range(0, len(pH)):
+            exp[i] = float(pH[i]) - float(pKb)
+            X[i] = (C[i]/(1+10**exp[i]) - KH*PC[i])
+
+    [slope, intercept, r, p, se] = linregress(X, Y)
+    return [slope, intercept, r**0.5, p, se] 
+
+
 def Hydrolysis_RMSE(khyd, D, XTin, XT):
-    # Equation for hydrolysis
-    # D*(XTin - XT) = khyd * XT
-    # y = m * x + b
-    # y = D*(XTin - XT) | x = XT | m = khyd
+    """
+    Equation for hydrolysis
+    D*(XTin - XT) = khyd * XT
+    y = m * x + b
+    y = D*(XTin - XT) | x = XT | m = khyd
+    """
     
     y = np.empty(len(XT))
     m = np.empty(len(XT))
@@ -71,10 +102,12 @@ def Hydrolysis_RMSE(khyd, D, XTin, XT):
     return RMSE(y, mx)
 
 def Acidogenesis_RMSE(k1, alpha, D, S1, X1, khyd, Sin, XT):
-    # Equation for acidogenesis
-    # D*(Sin - S1) + khyd * XT = k1 * alpha * D * X1
-    # y = m * x + b
-    # y = D*(Sin - S1) + khyd * XT | x = alpha * D * X1 | m = k1
+    """
+    Equation for acidogenesis
+    D*(Sin - S1) + khyd * XT = k1 * alpha * D * X1
+    y = m * x + b
+    y = D*(Sin - S1) + khyd * XT | x = alpha * D * X1 | m = k1
+    """
     
     y = np.empty(len(S1))
     x = np.empty(len(S1))
@@ -88,3 +121,22 @@ def Acidogenesis_RMSE(k1, alpha, D, S1, X1, khyd, Sin, XT):
         mx[i] = m[i] * x[i]
     
     return RMSE(y, mx)
+
+def Acidogenesis_LIN(alpha, D, S1, X1, khyd, Sin, XT):
+    """
+    Equation for acidogenesis
+    D*(Sin - S1) + khyd * XT = k1 * alpha * D * X1
+    y = m * x + b
+    y = D*(Sin - S1) + khyd * XT | x = alpha * D * X1 | m = k1
+    """
+    
+    Y = np.empty(len(S1))
+    X = np.empty(len(S1))
+    
+    for i in range(0, len(S1)):
+        Y[i] = D[i]*(Sin - S1[i]) + khyd * XT[i]
+        X[i] = alpha * D[i] * X1[i]
+    
+    [slope, intercept, r, p, se] = linregress(X, Y)
+    
+    return [slope, intercept, r**0.5, p, se]
--- minimOpt.py
+++ minimOpt.py
@@ -34,6 +34,8 @@ def main():
     CO2_min = minimize(solver.Carbon_Dioxide_RMSE, x0 = (gs,), args = (C, pH, KH, PC, Kb, qC), bounds = bd, tol = 1e-10, constraints=r2_CO2)
     XT_min  = minimize(solver.Hydrolysis_RMSE, x0 = (gs,), args = (D, XTin, XT), bounds = bd, tol = 1e-10, constraints=r2_XT)
     S1_min  = minimize(solver.Acidogenesis_RMSE, x0 = (gs,), args = (alpha, D, S1, X1, XT_min.x, S1in, XT), bounds = bd, tol = 1e-10, constraints=r2_S1)
+    [kLa, q, r2, p_val, err] = solver.Carbon_Dioxide_LIN(C, pH, KH, PC, Kb, qC)
+    [k1, q1, r21, p_val1, err1] = solver.Acidogenesis_LIN(alpha, D, S1, X1, XT_min.x, S1in, XT)
     
     # Model evaluation
     qM_Model = np.empty(len(D))
@@ -43,21 +45,23 @@ def main():
     
     for i in range(0, len(D)):
         qM_Model[i] = rm.qMeval(CH4_min.x, alpha, D[i], X2[i])
-        qC_Model[i] = rm.qCeval(CO2_min.x, C[i], pH[i], KH, PC[i], Kb)
+        qC_Model[i] = rm.qCeval(kLa, C[i], pH[i], KH, PC[i], Kb)
         XT_Model[i] = rm.XTeval(XT_min.x, D[i], XTin)
-        S1_Model[i] = rm.S1eval(S1_min.x, alpha, D[i], X1[i], XT_min.x, S1in, XT[i])
+        S1_Model[i] = rm.S1eval(k1, alpha, D[i], X1[i], XT_min.x, S1in, XT[i])
 
     # print out results
-    fprint('k6', CH4_min.x, CH4_min.success, CH4_min.nit, CH4_min.fun, [qM, qM_Model])
-    fprint('kLa', CO2_min.x, CO2_min.success, CO2_min.nit, CO2_min.fun, [qC, qC_Model])
-    fprint('khyd', XT_min.x, XT_min.success, XT_min.nit, XT_min.fun, [XT, XT_Model])
-    fprint('k1', S1_min.x, S1_min.success, S1_min.nit, S1_min.fun, [S1, S1_Model])
+    fprint('k6-RMSE', CH4_min.x, CH4_min.success, CH4_min.nit, CH4_min.fun, [qM, qM_Model])
+    fprint('kLa-RMSE', CO2_min.x, CO2_min.success, CO2_min.nit, CO2_min.fun, [qC, qC_Model])
+    fprint('kLa-LIN', kLa, 'NO-INFO', err, r2, [qC, rm.qCeval(kLa, C, pH, KH, PC, Kb)])
+    fprint('khyd-RMSE', XT_min.x, XT_min.success, XT_min.nit, XT_min.fun, [XT, XT_Model])
+    fprint('k1-RMSE', S1_min.x, S1_min.success, S1_min.nit, S1_min.fun, [S1, S1_Model])
+    fprint('k1-LIN', k1, 'NO-INFO', err, r2, [S1, S1_Model])
 
     # plot results
     legend = ['Experimental data', 'Model']
     regplot(1/D, [qM, qM_Model], 'time [days]', 'qM [g/s]', 'Methane production regression', legend, False)
     regplot(1/D, [qC, qC_Model], 'time [days]', 'qC [g/s]', 'Carbon Dioxide production regression', legend, False)
-    regplot(1/D, [XT, XT_Model], 'time [days]', 'XT [kg/m3]', 'Particulate Hydrolysis regression', legend, True)
+    regplot(1/D, [XT, XT_Model], 'time [days]', 'XT [kg/m3]', 'Particulate Hydrolysis regression', legend, False)
     regplot(1/D, [S1, S1_Model], 'time [days]', 'S1 [kg/m3]', 'Acidogenesis regression', legend, True)
 
     return 0;
