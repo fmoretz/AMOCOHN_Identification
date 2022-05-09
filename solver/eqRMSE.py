4b3c298aadba29f514ea13d345404037a4a32bdd
Federico Moretta <federicomoretta97@gmail.com>
2022-05-09 17:11:38 +0200

    new regression algorithm uploaded

--- solver/eqRMSE.py
+++ solver/eqRMSE.py
@@ -2,7 +2,6 @@
 import numpy as np
 import solver.RegModel as rm
 from typing import List
-from scipy.stats import linregress
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 
